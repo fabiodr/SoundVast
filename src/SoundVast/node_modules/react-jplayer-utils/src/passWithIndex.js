8d6b5ba2bff22c70dd3eb4ab56a28e11eed17abb
Martin Dawson <u1356770@gmail.com>
2017-07-19 21:52:13 +0100

    fixed gitignore

--- src/SoundVast/node_modules/react-jplayer-utils/src/passWithIndex.js
+++
@@ -1,8 +0,0 @@
-import { compose, withContext } from 'recompose';
-import PropTypes from 'prop-types';
-
-export default component => compose(
-  withContext({
-    index: PropTypes.number,
-  }, ({ index }) => ({ index }),
-)(component));
