0d76eb9c88ddddf6bc2aeee6a630f45fe63dc0d9
u1356770@gmail.com <u1356770@gmail.com>
2018-03-01 18:10:45 +0100

    test stage env

--- src/SoundVast/wwwroot/components/header/header.jsx
+++ src/SoundVast/wwwroot/components/header/header.jsx
@@ -42,7 +42,7 @@ const Header = ({
         </Dropdown>)
         : [
           <ModalButton key={0} styleName="secondary" className={styles.navButton} modalId="login">
-            Login
+            Logind
           </ModalButton>,
           <ModalButton key={1} styleName="secondary" className={styles.navButton} modalId="register">
             Register
@@ -50,7 +50,7 @@ const Header = ({
       }
       <Dropdown
         titleCallback={onClick => (
-          <Button title="Legald" className={styles.dropdownTitle} styleName="secondary" onClick={onClick}>
+          <Button title="Legal" className={styles.dropdownTitle} styleName="secondary" onClick={onClick}>
             <LegalIcon className={styles.legalIcon} />
           </Button>
         )}
