e1d10ff3b0776d78ddc8f1a0b1256cb765402fef
u1356770@gmail.com <u1356770@gmail.com>
2018-03-02 15:36:10 +0100

    testing staging

--- src/SoundVast/wwwroot/components/header/header.jsx
+++ src/SoundVast/wwwroot/components/header/header.jsx
@@ -42,7 +42,7 @@ const Header = ({
         </Dropdown>)
         : [
           <ModalButton key={0} styleName="secondary" className={styles.navButton} modalId="login">
-            Logind
+            Logindd
           </ModalButton>,
           <ModalButton key={1} styleName="secondary" className={styles.navButton} modalId="register">
             Register
